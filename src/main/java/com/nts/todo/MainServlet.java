c325d00c39533caf06d0e62bfa25e23a31dd3c57
USER <USER@AD01087533>
2019-07-08 14:11:11 +0900

    TodoAddServlet 작성 및 main.jsp 수정

---
+++ src/main/java/com/nts/todo/MainServlet.java
@@ -0,0 +1,30 @@
+package com.nts.todo;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.nts.todo.dao.TodoDao;
+import com.nts.todo.dto.TodoDto;
+
+@WebServlet("/main")
+public class MainServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void service(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		TodoDao todoDao = new TodoDao();
+		List<TodoDto> todoList = todoDao.getTodos();
+		request.setAttribute("todoList", todoList);
+		RequestDispatcher requestDispatcher = request.getRequestDispatcher("/WEB-INF/main.jsp");
+		requestDispatcher.forward(request, response);
+	}
+
+}
---
+++ src/main/java/com/nts/todo/TodoAddServlet.java
@@ -0,0 +1,44 @@
+package com.nts.todo;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.nts.todo.dao.TodoDao;
+import com.nts.todo.dto.TodoDto;
+
+@WebServlet("/todoadd")
+public class TodoAddServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// 매개변수로 전달되는 데이터의 한글 처리
+		request.setCharacterEncoding("utf-8");
+		response.setContentType("text/plain;charset=UTF-8");
+
+		String title = request.getParameter("title");
+		String name = request.getParameter("name");
+		int sequence = Integer.parseInt(request.getParameter("sequence"));
+
+		TodoDto todo = new TodoDto();
+		todo.setTitle(title);
+		todo.setName(name);
+		todo.setSequence(sequence);
+		TodoDao todoDao = new TodoDao();
+		int insertCount = todoDao.addTodo(todo);
+		if (insertCount > 0) {
+			response.sendRedirect("/main");
+		} else {
+			PrintWriter out = response.getWriter();
+			out.write("error occured on insert");
+			out.close();
+		}
+	}
+
+}
