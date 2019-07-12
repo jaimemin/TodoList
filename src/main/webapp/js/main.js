88489b723bb7206fd059a456fcdca0124dd96f19
USER <USER@AD01087533>
2019-07-12 16:57:27 +0900

    es6 적용하여 arrow function 이용

--- src/main/webapp/js/main.js
+++ src/main/webapp/js/main.js
@@ -10,9 +10,9 @@ function ajax(event) {
 	let parameter = `status=${status}&id=${id}`;
 
 	let xmlHttpRequest = new XMLHttpRequest();
-	xmlHttpRequest.onreadystatechange = function() {
-		if (this.readyState === 4) {
-			if (this.status >= 400){
+	xmlHttpRequest.onreadystatechange = () => {
+		if (xmlHttpRequest.readyState === 4) {
+			if (xmlHttpRequest.status >= 400){
 				alert("에러 발생! 잠시 후에 서비스를 이용해주세요.")
 				return;
 			}
@@ -28,8 +28,9 @@ function ajax(event) {
 			document.querySelector('#' + changedStatus).appendChild(todoCard);
 		}
 	}
-
-	xmlHttpRequest.open('POST', "/TodoList/todo-update");
+	
+	// async = true
+	xmlHttpRequest.open('POST', "/TodoList/todo-update", true);
 	// 키와 값을 =와 함께 표현하고 &의 묶음으로 표현하는 데이터 구조
 	xmlHttpRequest.setRequestHeader("Content-type",
 			"application/x-www-form-urlencoded; charset=UTF-8");
